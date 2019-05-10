160cef8be1dff68e73f60a7cebd09778a98013e8
Sergio Rosa <scarlosjazz@gmail.com>
2019-05-10 11:58:21 -0400

    Setting initial routes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { Component, AsyncStorage } from 'react';
+
 
 import '~/config/ReactotronConfig';
 
-import Routes from './routes';
+import creatNavigator from './routes';
 
-const App = () => <Routes />;
+export default class App extends Component {
+  state = {
+    userChecked: false,
+    userLogged: false,
+  };
 
-export default App;
+  async componentDidMount() {
+    const username = await AsyncStorage.getItem('@GitHuber:username');
+
+    this.setState({
+      userChecked: true,
+      userLogged: !!username,
+    });
+  }
+
+  render() {
+    const { userChecked, userLogged } = this.state;
+
+    if (!userChecked) return null;
+
+    const Routes = creatNavigator(userLogged);
+    return <Routes />;
+  }
+}
